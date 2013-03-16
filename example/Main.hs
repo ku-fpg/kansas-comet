fe2524a63a026f7341409ae5e26720502cf96107
Andy Gill <andygill@ku.edu>
2013-03-15 23:16:19 -0500

    Fixing the example to work without the event stuff.

--- example/Main.hs
+++ example/Main.hs
@@ -12,7 +12,8 @@ import Web.KansasComet as KC
 import Data.Default
 import Data.Map (Map)
 import Control.Monad
---import Control.Applicative
+import Control.Applicative ((<$>),(<*>))
+import qualified Control.Applicative as A
 import Control.Concurrent
 import Control.Concurrent.STM
 import Data.Semigroup
@@ -37,24 +38,35 @@ main = scotty 3000 $ do
     connect opts web_app
 
 opts :: KC.Options
-opts = def { prefix = "/example", verbose = 0 }
+opts = def { prefix = "/example", verbose = 3 }
 
 -- This is run each time the page is first accessed
 web_app :: Document -> IO ()
 web_app doc = do
-    registerEvents doc "body" (slide <> click)
+    send doc $ unlines
+        [ "$('body').on('slide', '.slide', function (event,aux) {"
+        , "$.kc.reply(0,{eventname: 'slide', count: aux.value });"
+        , "});"
+        ]
+    send doc $ unlines
+        [ "$('body').on('click', '.click', function (event,aux) {"
+        , "$.kc.reply(0,{eventname: 'click', id: $(this).attr('id'), pageX: event.pageX, pageY: event.pageY });"
+        , "});"
+        ]
     forkIO $ control doc 0
     return ()
 
 control :: Document -> Int -> IO ()
 control doc model = do
-    Just res <- waitForEvent doc "body" (slide <> click)
-    case res of
-        Slide _ n                      -> view doc n
-        Click "up"    _ _ | model < 25 -> view doc (model + 1)
-        Click "down"  _ _ | model > 0  -> view doc (model - 1)
-        Click "reset" _ _              -> view doc 0
-        _ -> control doc model
+    res <- getReply doc 0
+    case parse parseEvent res of
+           Success evt -> case evt of
+                   Slide n                        -> view doc n
+                   Click "up"    _ _ | model < 25 -> view doc (model + 1)
+                   Click "down"  _ _ | model > 0  -> view doc (model - 1)
+                   Click "reset" _ _              -> view doc 0
+                   _ -> control doc model
+           _ -> control doc model
 
 view :: Document -> Int -> IO ()
 view doc n = do
@@ -69,18 +81,21 @@ view doc n = do
 
 fib n = if n < 2 then 1 else fib (n-1) + fib (n-2)
 
-data Event = Slide String Int
+parseEvent (Object v) = (do
+                e :: String <- v .: "eventname"
+                n <- v .: "count"
+                if e == "slide" then return $ Slide n
+                                else mzero) A.<|>
+                                (do
+                e :: String <- v .: "eventname"
+                tag :: String <- v .: "id"
+                x :: Int <- v .: "pageX"
+                y :: Int <- v .: "pageY"
+                if e == "click" then return $ Click tag x y
+                                else mzero)
+          -- A non-Object value is of the wrong type, so fail.
+parseEvent _          = mzero
+
+data Event = Slide Int
            | Click String Int Int
     deriving (Show)
-
-slide = event "slide" Slide
-            <&> "id"      .= "$(widget).attr('id')"
-            <&> "count"   .= "aux.value"
-
-click = event "click" Click
-            <&> "id"      .= "$(widget).attr('id')"
-            <&> "pageX"   .=  "event.pageX"
-            <&> "pageY"   .=  "event.pageY"
-
-events = slide <> click
-
